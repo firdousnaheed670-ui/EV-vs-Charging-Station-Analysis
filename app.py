cdcbe97b7bc92b8c7ee9a1f5ee6ace1b8c210191
Firdous Naheed <firdousnaheed670@gmail.com>
2026-06-06 18:43:54 +0530

    Change merge method to inner join for datasets

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ ev_df = pd.read_excel("data/electric_vehicle.xlsx")
 station_df = pd.read_csv("data/charging_stations.csv") 
 
 # Merge on common columns (e.g., state + year)
-df_comb = pd.merge(ev_df, station_df, on=["State", "Year"])
+df_comb = pd.merge(ev_df, station_df, on=["State", "Year"],how="inner")
 
 # Show preview
 st.write("Combined Dataset Preview:", df_comb.head())
